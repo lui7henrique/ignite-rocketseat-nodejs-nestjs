a3e70f69c0262fd765130ed577fda30e3f15dcbf
Luiz Henrique <7henrique18@gmail.com>
2023-10-16 20:23:35 -0300

    feat: setup prisma service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,24 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
+import { PrismaService } from './prisma/prisma.service'
 
 @Controller()
 export class AppController {
   // eslint-disable-next-line no-useless-constructor
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private prisma: PrismaService,
+  ) {}
 
-  @Get()
-  getHello(): string {
+  @Get('/hello')
+  index(): string {
     return this.appService.getHello()
+  }
+
+  @Get('/teste')
+  async store() {
+    const users = await this.prisma.user.findMany()
+    console.log({ users })
+    return users
   }
 }
